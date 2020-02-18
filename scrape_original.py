13a574fabacb453f1ad30b5a832089294fc175d9
amulya7999 <59879253+amulya7999@users.noreply.github.com>
2020-02-18 21:11:28 +0530

    added for loop for multiple pages

--- scrape_original.py
+++ scrape_original.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-
 url="https://www.osmania.ac.in/res07/20190318.jsp"
 
 headers={
@@ -13,17 +12,41 @@ headers={
 }
 
 c=requests.Session()
+rollno=input("enter roll no")
+enter = int(input('enter subject code'))
+for x in range(3):
+    number=int(rollno)
+    number = number + 1
+    rollno = str(number)
+    data={'mbstatus':'SEARCH','htno':rollno,'Submit.x':'32','Submit.y':'14'}
+    res=c.post(url,data=data)
+    print(rollno)
+    s = BeautifulSoup(res.content, 'html.parser')
+#title=s.find_all('title')[0].get_text()
+#print(title)
+#subtitle=s.find_all('p')[0].get_text()
+#print(subtitle)
+#date=s.find_all('p')[1].get_text()
+#print(date)
+#subtitle2=s.find_all('td')[2].get_text()
+#print(subtitle2)
+    for i in (8,13):
+        subject = int(s.find_all('table')[1].find_all('tr')[i].find_all('td')[0].get_text())
+        if subject == enter:
+            continue
+        continue
+        print(subject)
+    sbname = s.find_all('table')[1].find_all('tr')[i].find_all('td')[1].get_text()
+    name = s.find_all('table')[1].find_all('tr')[1].find_all('td')[1].get_text()
+    credit =  float(s.find_all('table')[1].find_all('tr')[i].find_all('td')[2].get_text())
+    grade =  int(s.find_all('table')[1].find_all('tr')[i].find_all('td')[3].get_text())
+    gradez = s.find_all('table')[1].find_all('tr')[i].find_all('td')[4].get_text()  
+    print(grade)
+    print(gradez)
+    print(subject)
+    print(name)
+print(sbname)
+    #print(credit)
+    
 
 
-rollno="245318737090"
-data={'mbstatus':'SEARCH','htno':rollno,'Submit.x':'32','Submit.y':'14'}
-res=c.post(url,data=data)
-s = BeautifulSoup(res.content, 'html.parser')
-title=s.find_all('title')[0].get_text()
-print(title)
-subtitle=s.find_all('p')[0].get_text()
-print(subtitle)
-date=s.find_all('p')[1].get_text()
-print(date)
-subtitle2=s.find_all('td')[2].get_text()
-print(subtitle2)
